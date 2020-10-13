e60f9f572bb1bbb060a6e5674a247eff07260fba
Gio Palacino <gio@palacino.net>
2020-10-13 16:15:41 -0500

    Rm git tests from code climate

--- code-climate/tests/failure.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { TaskMockRunner } from 'azure-pipelines-task-lib/mock-run';
-
-const taskPath = path.join(__dirname, '../index.ts');
-const tmr = new TaskMockRunner(taskPath);
-tmr.setAnswers({
-  which: { git: (undefined as unknown) as string },
-});
-tmr.run();
