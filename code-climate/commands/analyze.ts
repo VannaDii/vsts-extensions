a3fe3565eb24c109bf415b7f4177840f6ee66b6a
Gio Palacino <gio@palacino.net>
2020-10-19 11:13:13 -0500

    Changes codeclimate analyze output handling

--- code-climate/commands/analyze.ts
+++ code-climate/commands/analyze.ts
@@ -13,46 +13,33 @@ export async function analyze(config: TaskConfig) {
   }
   tl.mkdirP(path.dirname(config.outputPath));
 
-  const tempFilePath = `${config.outputPath}.tmp`;
-
   const defaultTimeout = 900;
   const defaultMemory = 1024000000;
-  const outputStream = fs.createWriteStream(tempFilePath, { encoding: 'utf8', autoClose: true });
+  const outputStream = fs.createWriteStream(config.outputPath, { encoding: 'utf8', autoClose: true });
   const relativeSourcePath = path.join('.', path.relative(config.configFilePath, config.sourcePath));
   const execOptions: IExecSyncOptions = {
     cwd: config.configFilePath,
-    outStream: outputStream,
     env: {
       CODECLIMATE_DEBUG: config.debug ? '1' : undefined,
       CONTAINER_TIMEOUT_SECONDS: config.engineTimeout !== defaultTimeout ? config.engineTimeout.toString() : undefined,
       ENGINE_MEMORY_LIMIT_BYTES: config.memLimit !== defaultMemory ? config.memLimit.toString() : undefined,
     },
   };
-  const result = tl
+  const result = await tl
     .tool(codeClimate.path)
     .arg('analyze')
     .arg('-f')
     .arg(config.analysisFormat)
     .arg(relativeSourcePath)
-    .execSync(execOptions);
+    .on('stdline', (line: string) => {
+      if (!line.startsWith('[command]')) {
+        outputStream.write(`${line}\n`);
+      }
+    })
+    .on('errline', (line: string) => console.error(line))
+    .exec(execOptions);
 
-  let lineIndex = 0;
-  const tempFileStream = fs.createReadStream(tempFilePath, { encoding: 'utf8' });
-  const finalStream = fs.createWriteStream(config.outputPath, { encoding: 'utf8', autoClose: true });
-  lineReader.eachLine(tempFileStream, (line) => {
-    if (lineIndex === 0) return;
-    finalStream.write(`${line}\n`);
-    lineIndex++;
-  });
-  finalStream.close();
-  tempFileStream.close();
-  fs.unlinkSync(tempFilePath);
-
-  if (result.code !== 0) {
-    return tl.setResult(
-      tl.TaskResult.Failed,
-      `Code: ${result.code}\n\nErrors:\n${result.stderr}\n\nOutput:\n${result.stdout}`,
-      true
-    );
+  if (result !== 0) {
+    return tl.setResult(tl.TaskResult.Failed, `Code: ${result} - See output for details`, true);
   }
 }
