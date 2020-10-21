8f37fb9ef2742a7ec414803b6ef15e770ffd568c
Gio Palacino <gio@palacino.net>
2020-10-21 17:13:01 -0500

    Adds error handling to code-climate

--- code-climate/workItems.ts
+++ code-climate/workItems.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 import MarkdownIt from 'markdown-it';
+import * as tl from 'azure-pipelines-task-lib/task';
 import MarkdownItHighlightJs from 'markdown-it-highlightjs';
-import Axios, { AxiosRequestConfig } from 'axios';
+import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import {
   AnalysisIssue,
   QueryCondition,
@@ -41,100 +42,128 @@ export class WorkItemClient {
     };
   }
 
-  async create(type: WorkItemType, issue: AnalysisIssue, component: string, buildVersion: string) {
-    const workItemUrl = path.join(this.witUrls.WorkItems, `$${type.toLowerCase()}`);
-    const titlePrefix = issue.check_name[0].toUpperCase() + issue.check_name.replace('-', ' ').slice(1);
-    const basicDesc = `<ol><li>Open ${component} > ${issue.location.path} and observe lines ${issue.location.positions.begin.line} - ${issue.location.positions.end.line}.</li></ol>`;
-    const extDesc = this.markdown.render(issue.content.body);
-    const ops = [
-      {
-        op: 'add',
-        path: '/fields/CodeClimate.Fingerprint',
-        value: issue.fingerprint,
-        from: null,
-      },
-      {
-        op: 'add',
-        path: '/fields/System.Tags',
-        value: ['Code Climate', ...issue.categories, issue.check_name].join(','),
-        from: null,
-      },
-      {
-        op: 'add',
-        path: '/fields/System.Title',
-        value: `${titlePrefix} in ${component} > ${issue.location.path}`,
-        from: null,
-      },
-      {
-        op: 'add',
-        path: '/fields/System.State',
-        value: 'New',
-        from: null,
-      },
-      {
-        op: 'add',
-        path: '/fields/Microsoft.VSTS.Build.FoundIn',
-        value: `${component}_${buildVersion}`,
-        from: null,
-      },
-      {
-        op: 'add',
-        path: '/fields/Microsoft.VSTS.Scheduling.Effort',
-        value: Math.max(1, issue.remediation_points / 10000).toString(),
-        from: null,
-      },
-      {
-        op: 'add',
-        path: '/fields/Microsoft.VSTS.TCM.ReproSteps',
-        value: `${issue.description}<br/><br/>${basicDesc}<br/><br/>${extDesc}`,
-        from: null,
-      },
-    ];
-    const result = await Axios.patch<WorkItem>(workItemUrl, ops, { ...this.axiosConfig });
-    if (result.status !== 200) {
-      throw new Error(result.statusText);
+  private maybeLogError<T>(response: AxiosResponse<T>) {
+    if (response.status !== 200) {
+      tl.debug(`Error ${response.status}: ${response.statusText}`);
+      tl.debug(
+        typeof response.data !== 'object' ? ((response.data as unknown) as string) : JSON.stringify(response.data)
+      );
+      return true;
     }
-    return result.data;
+    return undefined;
   }
 
-  async update(id: number, ...ops: WorkItemPatch): Promise<WorkItem> {
-    const workItemUrl = path.join(this.witUrls.WorkItems, id.toString());
-    const result = await Axios.patch<WorkItem>(workItemUrl, ops, { ...this.axiosConfig });
-    if (result.status !== 200) {
-      throw new Error(result.statusText);
+  private async tryCatch<T>(op: () => Promise<T>): Promise<T | undefined> {
+    try {
+      return await op();
+    } catch (error) {
+      tl.error(`${error.name}: ${error.message}\n${error.stack}`);
     }
-    return result.data;
+    return undefined;
+  }
+
+  async create(type: WorkItemType, issue: AnalysisIssue, component: string, buildVersion: string) {
+    return this.tryCatch(async () => {
+      const workItemUrl = path.join(this.witUrls.WorkItems, `$${type.toLowerCase()}`);
+      const titlePrefix = issue.check_name[0].toUpperCase() + issue.check_name.replace('-', ' ').slice(1);
+      const basicDesc = `<ol><li>Open ${component} > ${issue.location.path} and observe lines ${issue.location.positions.begin.line} - ${issue.location.positions.end.line}.</li></ol>`;
+      const extDesc = this.markdown.render(issue.content.body);
+      const ops = [
+        {
+          op: 'add',
+          path: '/fields/CodeClimate.Fingerprint',
+          value: issue.fingerprint,
+          from: null,
+        },
+        {
+          op: 'add',
+          path: '/fields/System.Tags',
+          value: ['Code Climate', ...issue.categories, issue.check_name].join(','),
+          from: null,
+        },
+        {
+          op: 'add',
+          path: '/fields/System.Title',
+          value: `${titlePrefix} in ${component} > ${issue.location.path}`,
+          from: null,
+        },
+        {
+          op: 'add',
+          path: '/fields/System.State',
+          value: 'New',
+          from: null,
+        },
+        {
+          op: 'add',
+          path: '/fields/Microsoft.VSTS.Build.FoundIn',
+          value: `${component}_${buildVersion}`,
+          from: null,
+        },
+        {
+          op: 'add',
+          path: '/fields/Microsoft.VSTS.Scheduling.Effort',
+          value: Math.max(1, issue.remediation_points / 10000).toString(),
+          from: null,
+        },
+        {
+          op: 'add',
+          path: '/fields/Microsoft.VSTS.TCM.ReproSteps',
+          value: `${issue.description}<br/><br/>${basicDesc}<br/><br/>${extDesc}`,
+          from: null,
+        },
+      ];
+      const result = await Axios.patch<WorkItem>(workItemUrl, ops, { ...this.axiosConfig });
+      const isError = this.maybeLogError(result);
+      if (isError) throw new Error(result.statusText);
+
+      return result.data;
+    });
+  }
+
+  async update(id: number, ...ops: WorkItemPatch) {
+    return this.tryCatch(async () => {
+      const workItemUrl = path.join(this.witUrls.WorkItems, id.toString());
+      const result = await Axios.patch<WorkItem>(workItemUrl, ops, { ...this.axiosConfig });
+      const isError = this.maybeLogError(result);
+      if (isError) throw new Error(result.statusText);
+
+      return result.data;
+    });
   }
 
   async get(fields: string[], ...ids: number[]) {
-    const result = await Axios.post<WorkItemBatch>(
-      this.witUrls.WorkItemsBatch,
-      { ids, fields },
-      { ...this.axiosConfig }
-    );
-    if (result.status !== 200) {
-      throw new Error(result.statusText);
-    }
-    return result.data;
+    return this.tryCatch(async () => {
+      const result = await Axios.post<WorkItemBatch>(
+        this.witUrls.WorkItemsBatch,
+        { ids, fields },
+        { ...this.axiosConfig }
+      );
+      const isError = this.maybeLogError(result);
+      if (isError) throw new Error(result.statusText);
+
+      return result.data;
+    });
   }
 
   async query(fields: string[], conditions: QueryCondition[]) {
-    const fieldSet = fields.map((v) => `[${v}]`).join(', ');
-    const conditionSet = conditions.map((c) => `[${c.fieldName}] ${c.operator} ${c.value}`).join(' AND ');
-    const wiqlQuery = `Select ${fieldSet} From WorkItems Where ${conditionSet}`;
-    const result = await Axios.post<WorkItemQueryResult>(
-      this.witUrls.WIQL,
-      { query: wiqlQuery },
-      { ...this.axiosConfig }
-    );
-    if (result.status !== 200) {
-      throw new Error(result.statusText);
-    }
-    return result.data;
+    return this.tryCatch(async () => {
+      const fieldSet = fields.map((v) => `[${v}]`).join(', ');
+      const conditionSet = conditions.map((c) => `[${c.fieldName}] ${c.operator} ${c.value}`).join(' AND ');
+      const wiqlQuery = `Select ${fieldSet} From WorkItems Where ${conditionSet}`;
+      const result = await Axios.post<WorkItemQueryResult>(
+        this.witUrls.WIQL,
+        { query: wiqlQuery },
+        { ...this.axiosConfig }
+      );
+      const isError = this.maybeLogError(result);
+      if (isError) throw new Error(result.statusText);
+
+      return result.data;
+    });
   }
 
   async fieldEnsure(fieldName: string, factory: (fieldName: string) => WorkItemField) {
-    let field!: WorkItemField;
+    let field!: WorkItemField | undefined;
     try {
       field = await this.fieldGet(fieldName);
     } catch (error) {
@@ -145,22 +174,26 @@ export class WorkItemClient {
   }
 
   async fieldCreate(field: WorkItemField) {
-    const result = await Axios.post<WorkItemField>(this.witUrls.Fields, field, { ...this.axiosConfig });
-    if (result.status !== 200) {
-      throw new Error(result.statusText);
-    }
-    return result.data;
+    return this.tryCatch(async () => {
+      const result = await Axios.post<WorkItemField>(this.witUrls.Fields, field, { ...this.axiosConfig });
+      const isError = this.maybeLogError(result);
+      if (isError) throw new Error(result.statusText);
+
+      return result.data;
+    });
   }
 
   async fieldGet(fieldName: string) {
-    const fieldUrl = path.join(this.witUrls.Fields, fieldName);
-    const [result, fallback] = await Promise.all([
-      Axios.get<WorkItemField>(fieldUrl, { ...this.axiosConfig }),
-      Axios.get<WorkItemField>(fieldUrl, { ...this.axiosConfigFallback }),
-    ]);
-    if (result.status !== 200 && fallback.status !== 200) {
-      throw new Error(result.statusText);
-    }
-    return result.status === 200 ? result.data : fallback.data;
+    return this.tryCatch(async () => {
+      const fieldUrl = path.join(this.witUrls.Fields, fieldName);
+      const [result, fallback] = await Promise.all([
+        Axios.get<WorkItemField>(fieldUrl, { ...this.axiosConfig }),
+        Axios.get<WorkItemField>(fieldUrl, { ...this.axiosConfigFallback }),
+      ]);
+      const isError = this.maybeLogError(result);
+      if (isError) throw new Error(result.statusText);
+
+      return result.status === 200 ? result.data : fallback.data;
+    });
   }
 }
