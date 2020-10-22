47f743b77c3391161cefed16ce056b0448eeed96
Gio Palacino <gio@palacino.net>
2020-10-22 09:52:42 -0500

    Adjust logging, adds correlation id

--- code-climate/workItems.ts
+++ code-climate/workItems.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import MarkdownIt from 'markdown-it';
 import * as tl from 'azure-pipelines-task-lib/task';
+import { customAlphabet } from 'nanoid/non-secure';
 import MarkdownItHighlightJs from 'markdown-it-highlightjs';
 import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import {
@@ -14,6 +15,8 @@ import {
   WorkItemType,
 } from './types';
 
+type Dictionary = { [key: string]: string | number | boolean | object };
+
 export class WorkItemClient {
   private readonly axiosConfig: AxiosRequestConfig;
   private readonly axiosConfigFallback: AxiosRequestConfig;
@@ -23,9 +26,10 @@ export class WorkItemClient {
     WorkItemsBatch: 'workitemsbatch',
     WorkItems: 'workitems',
   };
-  private markdown = new MarkdownIt({ linkify: true, typographer: true, xhtmlOut: true }).use(MarkdownItHighlightJs, {
+  private readonly markdown = new MarkdownIt({ linkify: true, typographer: true, xhtmlOut: true }).use(MarkdownItHighlightJs, {
     inline: true,
   });
+  private readonly nanoid = customAlphabet('1234567890abcdef', 10);
 
   constructor(collectionUrl: string, projName: string, accessToken: string) {
     this.axiosConfig = {
@@ -42,28 +46,19 @@ export class WorkItemClient {
     };
   }
 
-  private maybeLogError<T>(response: AxiosResponse<T>) {
-    if (response.status !== 200) {
-      tl.debug(`Error ${response.status}: ${response.statusText}`);
-      tl.debug(
-        typeof response.data !== 'object' ? ((response.data as unknown) as string) : JSON.stringify(response.data)
-      );
-      return true;
-    }
-    return undefined;
-  }
-
-  private async tryCatch<T>(op: () => Promise<T>): Promise<T | undefined> {
+  private async tryCatch<T>(op: (context: Dictionary) => Promise<T>): Promise<T | undefined> {
+    const context: Dictionary = { correlationId: this.nanoid() };
     try {
-      return await op();
+      return await op(context);
     } catch (error) {
+      tl.debug(`Context: ${JSON.stringify(context)}`);
       tl.error(`${error.name}: ${error.message}\n${error.stack}`);
     }
     return undefined;
   }
 
   async create(type: WorkItemType, issue: AnalysisIssue, component: string, buildVersion: string) {
-    return this.tryCatch(async () => {
+    return this.tryCatch(async (context) => {
       const workItemUrl = path.join(this.witUrls.WorkItems, `$${type.toLowerCase()}`);
       const titlePrefix = issue.check_name[0].toUpperCase() + issue.check_name.replace('-', ' ').slice(1);
       const basicDesc = `<ol><li>Open ${component} > ${issue.location.path} and observe lines ${issue.location.positions.begin.line} - ${issue.location.positions.end.line}.</li></ol>`;
@@ -112,51 +107,54 @@ export class WorkItemClient {
           from: null,
         },
       ];
+
+      context.scope = this.create.name;
+      context.url = workItemUrl;
       const result = await Axios.patch<WorkItem>(workItemUrl, ops, { ...this.axiosConfig });
-      const isError = this.maybeLogError(result);
-      if (isError) throw new Error(result.statusText);
 
       return result.data;
     });
   }
 
   async update(id: number, ...ops: WorkItemPatch) {
-    return this.tryCatch(async () => {
+    return this.tryCatch(async (context) => {
       const workItemUrl = path.join(this.witUrls.WorkItems, id.toString());
+
+      context.scope = this.update.name;
+      context.url = workItemUrl;
       const result = await Axios.patch<WorkItem>(workItemUrl, ops, { ...this.axiosConfig });
-      const isError = this.maybeLogError(result);
-      if (isError) throw new Error(result.statusText);
 
       return result.data;
     });
   }
 
   async get(fields: string[], ...ids: number[]) {
-    return this.tryCatch(async () => {
+    return this.tryCatch(async (context) => {
+      context.scope = this.get.name;
+      context.url = this.witUrls.WorkItemsBatch;
       const result = await Axios.post<WorkItemBatch>(
         this.witUrls.WorkItemsBatch,
         { ids, fields },
         { ...this.axiosConfig }
       );
-      const isError = this.maybeLogError(result);
-      if (isError) throw new Error(result.statusText);
 
       return result.data;
     });
   }
 
   async query(fields: string[], conditions: QueryCondition[]) {
-    return this.tryCatch(async () => {
+    return this.tryCatch(async (context) => {
       const fieldSet = fields.map((v) => `[${v}]`).join(', ');
       const conditionSet = conditions.map((c) => `[${c.fieldName}] ${c.operator} ${c.value}`).join(' AND ');
       const wiqlQuery = `Select ${fieldSet} From WorkItems Where ${conditionSet}`;
+
+      context.scope = this.query.name;
+      context.url = this.witUrls.WIQL;
       const result = await Axios.post<WorkItemQueryResult>(
         this.witUrls.WIQL,
         { query: wiqlQuery },
         { ...this.axiosConfig }
       );
-      const isError = this.maybeLogError(result);
-      if (isError) throw new Error(result.statusText);
 
       return result.data;
     });
@@ -174,24 +172,24 @@ export class WorkItemClient {
   }
 
   async fieldCreate(field: WorkItemField) {
-    return this.tryCatch(async () => {
+    return this.tryCatch(async (context) => {
+      context.scope = this.fieldCreate.name;
+      context.url = this.witUrls.Fields;
       const result = await Axios.post<WorkItemField>(this.witUrls.Fields, field, { ...this.axiosConfig });
-      const isError = this.maybeLogError(result);
-      if (isError) throw new Error(result.statusText);
 
       return result.data;
     });
   }
 
   async fieldGet(fieldName: string) {
-    return this.tryCatch(async () => {
+    return this.tryCatch(async (context) => {
       const fieldUrl = path.join(this.witUrls.Fields, fieldName);
+      context.scope = this.fieldGet.name;
+      context.url = fieldUrl;
       const [result, fallback] = await Promise.all([
         Axios.get<WorkItemField>(fieldUrl, { ...this.axiosConfig }),
         Axios.get<WorkItemField>(fieldUrl, { ...this.axiosConfigFallback }),
       ]);
-      const isError = this.maybeLogError(result);
-      if (isError) throw new Error(result.statusText);
 
       return result.status === 200 ? result.data : fallback.data;
     });
