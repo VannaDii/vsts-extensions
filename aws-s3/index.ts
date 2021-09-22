0fedff4878d684802a5174b3a31546f67d9ed1f7
Gio Palacino <gio@palacino.net>
2021-09-22 13:29:45 -0500

    Adds AWS S3 bucket managment

--- aws-s3/index.ts
+++ aws-s3/index.ts
@@ -1,88 +1,71 @@
 import path from 'path';
-import axios from 'axios';
+import S3 from 'aws-sdk/clients/s3';
 import * as tl from 'azure-pipelines-task-lib/task';
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-function coalesce(...values: (string | undefined)[]): string {
-  values = values || [];
-  return (values.find((v) => !!v && typeof v === 'string' && !!v.trim && v.trim().length > 0) as string).trim();
+async function doesBucketExist(client: S3, bucketName: string) {
+  try {
+    const result = await client.headBucket({ Bucket: bucketName }).promise();
+    return result?.$response?.httpResponse?.statusCode === 200;
+  } catch (error: any) {
+    tl.error(error);
+  }
+  return false;
 }
 
 (async () => {
   try {
-    // Init variables
-    const timeStamp = new Date().toISOString();
-    const releaseEnv = tl.getVariable('Release.EnvironmentName');
-    const agentStatus = tl.getVariable('Agent.JobStatus');
-    const gitShaShort = (tl.getVariable('Build.SourceVersion') as string).slice(0, 8);
-    const releaseStatus = tl.getVariable(`Release.Environments.${releaseEnv}.status`);
-    const usableStatus = coalesce(releaseStatus, agentStatus, 'unknown').toLowerCase();
-    const defaultStatus = { color: 10181046, status: usableStatus };
-    const colorStatuses: { [key: string]: { color: number; status: string } } = {
-      canceled: { color: 9807270, status: 'was canceled' },
-      failed: { color: 15158332, status: 'failed' },
-      succeeded: { color: 3066993, status: 'succeeded' },
-      succeededwithissues: { color: 15105570, status: 'succeeded with issues' },
-      unknown: { color: 3447003, status: 'was weird' },
-    };
-    const { color, status } = colorStatuses[usableStatus] || defaultStatus;
+    const ifNotExists = tl.getBoolInput('IfNotExists', false) || false;
+    const bucketName = tl.getInput('BucketName', true);
+    const actionType = tl.getInput('ActionType', true);
 
-    const channelId = tl.getInput('DiscordChannelId', true);
-    if (!channelId || channelId.length === 0) {
-      tl.setResult(tl.TaskResult.Failed, tl.loc('NoChannelId'));
+    if (!bucketName || bucketName.length === 0) {
+      tl.setResult(tl.TaskResult.Failed, tl.loc('NoBucketName'));
       return;
     } else {
-      tl.debug(`Using provided channel ID: '${channelId}'`);
+      tl.debug(`Using provided bucket name: '${bucketName}'`);
     }
-
-    const webhookKey = tl.getInput('DiscordWebhookKey', true);
-    if (!webhookKey || webhookKey.length === 0) {
-      tl.setResult(tl.TaskResult.Failed, tl.loc('NoWebhookKey'));
+    if (!actionType || actionType.length === 0) {
+      tl.setResult(tl.TaskResult.Failed, tl.loc('NoActionType'));
       return;
     } else {
-      tl.debug(`Using provided webhook key`);
+      tl.debug(`Using provided action type: '${actionType}'`);
     }
 
-    let customMessage = tl.getInput('DiscordCustomMessage', true);
-    if (!customMessage || customMessage.length === 0) {
-      tl.setResult(tl.TaskResult.Failed, tl.loc('NoCustomMessage'));
-      return;
-    } else {
-      try {
-        customMessage = customMessage
-          .replace(/\s{2,}/g, '')
-          .replace(/\r|\n/g, '')
-          .replace(/\$\(GitShaShort\)/gi, gitShaShort)
-          .replace(/\$\(DiscordJobStatus\)/gi, status)
-          .replace(/\$\(DiscordTimeStamp\)/gi, timeStamp)
-          .replace(/\$\(DiscordStatusColor\)/gi, color.toString());
-        JSON.parse(customMessage);
-        tl.debug(`Using provided custom message: '${customMessage}'`);
-      } catch (error) {
-        tl.error(error);
-        tl.error(customMessage);
-        tl.setResult(tl.TaskResult.Failed, tl.loc('CustomMessageNotJson'));
-        return;
-      }
+    const s3Client = new S3();
+    const bucketExists = await doesBucketExist(s3Client, bucketName);
+    switch (actionType) {
+      case 'Create':
+        {
+          if (bucketExists === false) {
+            tl.debug(`Creating '${bucketName}'`);
+            const result = await s3Client.createBucket({ Bucket: bucketName }).promise();
+            await s3Client.waitFor('bucketExists', { Bucket: bucketName }).promise();
+            tl.debug(`Bucket '${bucketName}' created in ${result.Location}`);
+          }
+        }
+        break;
+      case 'Destroy':
+        {
+          if (bucketExists === true) {
+            tl.debug(`Destroying '${bucketName}'`);
+            await s3Client.deleteBucket({ Bucket: bucketName }).promise();
+            await s3Client.waitFor('bucketNotExists', { Bucket: bucketName }).promise();
+            tl.debug(`Bucket '${bucketName}' is destroyed`);
+          }
+        }
+        break;
+      default:
+        tl.setResult(tl.TaskResult.Failed, tl.loc('InvalidActionType'));
     }
 
-    const webhookUrl = `https://discordapp.com/api/webhooks/${channelId}/${webhookKey}`;
-    const response = await axios.post(webhookUrl, customMessage, {
-      headers: { 'content-type': 'application/json' },
-    });
-
-    if (response.status >= 300) {
-      tl.setResult(tl.TaskResult.Failed, `${response.status}: ${response.statusText}`);
-      return;
-    }
-    tl.setResult(tl.TaskResult.Succeeded, `Posted to Discord channel #${channelId}`);
-
-  } catch (error) {
+    tl.setResult(tl.TaskResult.Succeeded, `${actionType} bucket succeeded!`);
+  } catch (error: any) {
     tl.error(error);
     tl.setResult(
       tl.TaskResult.Failed,
-      `Posting failed! ${error.name || 'Error'}: ${error.message || 'No message available'}\n${error.stack}`
+      `Bucket management failed! ${error.name || 'Error'}: ${error.message || 'No message available'}\n${error.stack}`
     );
   }
 })();
