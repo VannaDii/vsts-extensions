630ac3196ccf89fbfb213d2db829b15943cda5f6
Gio Palacino <gio@palacino.net>
2021-09-22 12:13:38 -0500

    Adds aws-s3 task

---
+++ aws-s3/tests/failure.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import axios, { pushResponse } from '../../__mocks__/axios';
+import { setMockRunnerInputs } from '../../jest.azure';
+import * as tmrm from 'azure-pipelines-task-lib/mock-run';
+
+const taskPath = path.join(__dirname, '../index.ts');
+const tmr = new tmrm.TaskMockRunner(taskPath);
+
+pushResponse({ status: 400, statusText: 'Bad request' });
+tmr.registerMock('axios', axios);
+
+setMockRunnerInputs(tmr, {
+  'Release.EnvironmentName': 'test',
+  'Agent.JobStatus': 'succeeded',
+  'Build.SourceVersion': '2ec09c17ba0a863c1f814619a95285d573c21245',
+  DiscordChannelId: 'test_channel_id',
+  DiscordWebhookKey: 'test_webhook_key',
+  DiscordCustomMessage:
+    '{ "content": "test discord message $(GitShaShort) $(DiscordJobStatus) $(DiscordTimeStamp) $(DiscordStatusColor)" }',
+}).run();
