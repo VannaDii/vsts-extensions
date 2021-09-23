07d95091c7a3b67f80e51c77a5547860dd19effa
Gio Palacino <gio@palacino.net>
2021-09-23 14:10:21 -0500

    fix(test) adjust jest settings

--- code-climate/__tests__/utils.test.ts
+++ code-climate/__tests__/utils.test.ts
@@ -1,9 +1,10 @@
-import { getEvenHash } from "../utils";
+import { getEvenHash } from '../utils';
 
 describe('Utility Tests', () => {
   const dataSet = new Array(94)
     .fill(0)
     .map((v, i) => String.fromCharCode(...new Array(v + i + 1).fill(0).map((v2, i2) => 33 + i2)));
+
   dataSet.forEach((set) => {
     it(`Creates Even, UnPadded, Hashes For ${set.length} Length Values`, () => {
       let hash!: string;
