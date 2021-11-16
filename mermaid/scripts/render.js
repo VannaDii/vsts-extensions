7d6c0a6c4f1c663e7929f797308326d34932dfad
Gio Palacino <gio@palacino.net>
2021-11-16 11:48:55 -0600

    fix(mermaid) fallback styles and setup for advanced render

--- mermaid/scripts/render.js
+++ mermaid/scripts/render.js
@@ -1,35 +1,12 @@
-let graphCounter = 0;
-const mermaidExtension = {
-  name: 'mermaid',
-  level: 'block',
-  start(src) {
-    return src.match(/^[@:]{3}\s*?mermaid\s*?$/gm)?.index;
-  },
-  tokenizer(src, tokens) {
-    const rule = /^[@:]{3}\smermaid\s*?^([\s\S]*?)^[@:]{3}\s*?$/gm;
-    const match = rule.exec(src);
-    if (match && src.substring(0, match.index).trim() === '') {
-      const token = {
-        type: 'mermaid',
-        raw: match[0], // Text to consume from the source
-        text: match[0], // Additional custom properties
-        tokens: [...match[1].split('\n')], // Array where child inline tokens will be generated
-        id: graphCounter++,
-      };
-      return token;
-    }
-  },
-  renderer(token) {
-    let graphSvg;
-    const grafDef = token.tokens.filter((t) => !t.startsWith('@')).join('\n');
-    mermaid.mermaidAPI.render(`graphDiv_${token.id}`, grafDef, (svg) => (graphSvg = svg));
-    return `<div class="mermaid">${graphSvg}</div>`;
-  },
-};
 const markedMermaidRenderer = {
   renderContent: function (rawContent, options) {
     const targetElement = document.getElementById('render-content-display');
     if (marked && marked.parse) {
+      marked.setOptions({
+        gfm: true,
+        xhtml: true,
+        headerIds: true,
+      });
       marked.use({ extensions: [mermaidExtension] });
       targetElement.innerHTML = marked.parse(rawContent);
     } else {
@@ -37,12 +14,45 @@ const markedMermaidRenderer = {
     }
   },
 };
-VSS.init({
+
+/* VSS.init({
   usePlatformScripts: true,
   usePlatformStyles: true,
   explicitNotifyLoaded: true,
 });
+
 VSS.ready(function () {
   VSS.register('marked_mermaid_renderer', (_) => markedMermaidRenderer);
   VSS.notifyLoadSucceeded();
-});
+}); */
+
+function renderTestContent() {
+  console.log('Rendering content');
+  markedMermaidRenderer.renderContent(`
+# Testing Content Rendering
+
+This is a simple sample to test various markdowns
+
+## Below is a Mermaid graph
+
+@@@ mermaid
+graph TD
+  A[Christmas] -->|Get money| B(Go shopping)
+  B --> C{Let me think}
+  C -->|One| D[Laptop]
+  C -->|Two| E[iPhone]
+  C -->|Three| F[fa:fa-car Car]
+@@@
+
+## Sometime we have table issues
+
+The above renders fine, but, the below does not render the grid lines:
+
+| C1 | C2 |
+|--|--|
+| 1 | A |
+| 2 | B |
+
+`);
+}
+
